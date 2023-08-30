86459d1396e6e4bfad7cff3b069d923ae9cb6e8a
kevin Pajaniandy <122272608+KevinPajaniandy@users.noreply.github.com>
2023-08-30 12:04:49 +0300

    Ajout post fetch post

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -5,6 +5,7 @@ let categoriesWorks =[ {
   "name": "Tous"
 }];
 
+
 checkIfConnected();
 loadFilters();
 loadProjects();
@@ -28,7 +29,6 @@ fetch('http://localhost:5678/api/works')
     alert("Une erreur est survenue ! Veuillez contacter l'administrateur! ");
   });
 }
-
 //Charger les filtres depuis l'API
 function loadFilters(){
     fetch('http://localhost:5678/api/categories')
@@ -92,7 +92,7 @@ function createFilters(tableauFilter){
 function createFigure(work){
   // On crée un élément HTML de type figure
     const figure = document.createElement('figure');
-    figure.setAttribute("id", work.id);
+    figure.setAttribute("id", "figure"+work.id);
   // On crée un élément HTML de type img et on lui attribue sa source et un texte alternatif
     const img = document.createElement('img');
     img.src = work.imageUrl;
@@ -100,6 +100,7 @@ function createFigure(work){
       // On crée un élément HTML de type figcaption pour le titre de l'image et on lui attribue le texte de la légende
     const figcaption = document.createElement('figcaption');
     figcaption.textContent = work.title;
+    figcaption.className ="figcaptionIndex"
   // On ajoute l'élément img à l'élément figure
     figure.appendChild(img);
   // On ajoute l'élément figcaption à l'élément figure
@@ -208,19 +209,4 @@ displayLogout.addEventListener("click",() =>{
   // redirect to offline homepage
   window.location.href = "/FrontEnd/index.html";
 });
-
-
-
- // Ajouter un bouton "Mode édition" avec un icône et un texte à la barre d'édition
- // coder la barre noir dans le html 
- // dans le js : basculer entre display block ou flex et display node pour afficher et cacher les element html 
-/* const editorBar = document.createElement('div');
-editorBar.className = 'editor-bar';
- editorBar.innerHTML = '<i class = \' fa-regular fa-pen-to-square \'></i> <p>Mode édition <span>publier les changements</span></p>';
- // Ajouter la barre d'édition au début du corps de la page
- document.body.prepend(editorBar);*/
- // Modifier le texte du bouton de connexion pour qu'il dise "Déconnexion"
- 
- // Suppression de tous les filtres
-
  
--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,70 +1,87 @@
 // Attend que le document soit prêt
 document.addEventListener("DOMContentLoaded", function () {
-// Cibler l'élément bouton pour ouvrir le modal
-let btns = document.querySelectorAll(".btnModifier");
+  // Cibler l'élément bouton pour ouvrir le modal
+  let btns = document.querySelectorAll(".btnModifier");
 
-// Cibler l'élément modal
-let modal = document.querySelector("#modal1");
+  // Cibler l'élément modal
+  let modal = document.querySelector("#modal1");
 
-// Cibler l'élément de fermeture (la croix)
-let closes = document.querySelectorAll(".js-modal-close");
+  // Cibler l'élément de fermeture (la croix)
+  let closes = document.querySelectorAll(".js-modal-close");
 
-// Fonction pour ouvrir le modal
-function openModal() {
-  modal.style.display = "block";
+  // Fonction pour ouvrir le modal
+  function openModal() {
+    modal.style.display = "block";
 
-// Sélectionne l'élément où les projets seront affichés.
-const projetsModal = modal.querySelector(".projets-modal"); 
+    // Sélectionne l'élément où les projets seront affichés.
+    const projetsModal = modal.querySelector(".projets-modal");
 
-// Vide le contenu actuel de la zone où les projets seront affichés.
-projetsModal.innerHTML = ""; 
+    // Vide le contenu actuel de la zone où les projets seront affichés.
+    projetsModal.innerHTML = "";
 
-// Boucle à travers chaque projet et crée un élément "figure" pour l'affichage.
-works.forEach((project) => {
-// Appelle la fonction "createFigure" pour chaque projet.
-const figure = createFigure(project);
-// Afficher le projet
-projetsModal.appendChild(figure); 
+    // Boucle à travers chaque projet et crée un élément "figure" pour l'affichage.
+    works.forEach((project) => {
+      // Appelle la fonction "createFigure" pour chaque projet.
+      const figure = createFigureModale(project);
+      // Afficher le projet
+      projetsModal.appendChild(figure);
     });
   }
 
-  // Coder une fonction que me cree un element figure comme suit 
-function createFigureModale(work){
-  // On crée un élément HTML de type figure
-    const figure = document.createElement('figure');
-    figure.setAttribute("id", "figureModale"+work.id);
-  // On crée un élément HTML de type img et on lui attribue sa source et un texte alternatif
-    const img = document.createElement('img');
+  // Coder une fonction que me cree un element figure comme suit
+  function createFigureModale(work) {
+    // On crée un élément HTML de type figure
+    const figure = document.createElement("figure");
+    figure.setAttribute("id", "figureModale" + work.id);
+    // On crée un élément HTML de type img et on lui attribue sa source et un texte alternatif
+    const img = document.createElement("img");
     img.src = work.imageUrl;
-    img.alt=work.title;
-      // On crée un élément HTML de type figcaption pour le titre de l'image et on lui attribue le texte de la légende
-    const figcaption = document.createElement('figcaption');
-    figcaption.textContent = "editer";
-  // On ajoute l'élément img à l'élément figure
-    figure.appendChild(img);
-  // On ajoute l'élément figcaption à l'élément figure
-    figure.appendChild(figcaption);
-
+    img.alt = work.title;
+    // On crée un élément HTML de type figcaption pour le titre de l'image et on lui attribue le texte de la légende
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = "éditer";
+    figcaption.className = "editer"
     //Ajouter l'icon trash et aussi un action listener dnas lequel tu affiche l'id de l'element
+    // Crée un élément HTML de type i pour l'icône de suppression
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fas fa-trash-alt delete-icon";
+    // Ajoute un gestionnaire d'événement pour la suppression lorsque l'icône est cliquée
+    deleteIcon.addEventListener("click", function () {
+      const value = confirm(
+        "Etes vous sur de bien vouloir supprimer le projet de numero:" +
+          work.id +
+          " ?"
+      );
+      if (value) {
+        // Appelle une fonction de suppression en utilisant l'ID de l'élément work
+        deleteWork(work.id);
+      }
+    });
+    // On ajoute l'élément img à l'élément figure
+    figure.appendChild(img);
+    // On ajoute l'élément figcaption à l'élément figure
+    figure.appendChild(figcaption);
+    // On ajoute l'élément sdeleteIcon à l'élément figure
+    figure.appendChild(deleteIcon);
 
     return figure;
-}
+  }
 
-// Fonction pour fermer le modal
-function closeModal() {
+  // Fonction pour fermer le modal
+  function closeModal() {
     modal.style.display = "none";
   }
-// Pour chaque bouton de la liste btns on va ajouter un listener pour ouvrir la modale
-btns.forEach((element) => {
-// Ouvrir le modal lorsqu'on clique sur le bouton
-  element.addEventListener("click", openModal);
+  // Pour chaque bouton de la liste btns on va ajouter un listener pour ouvrir la modale
+  btns.forEach((element) => {
+    // Ouvrir le modal lorsqu'on clique sur le bouton
+    element.addEventListener("click", openModal);
   });
-// Fermer le modal lorsqu'on clique sur la croix
-closes.forEach(close => {
-  close.addEventListener("click", closeModal);
-});
- 
-// Fermer le modal lorsqu'on clique en dehors de celui-ci (sur le fond semi-transparent)
+  // Fermer le modal lorsqu'on clique sur la croix
+  closes.forEach((close) => {
+    close.addEventListener("click", closeModal);
+  });
+
+  // Fermer le modal lorsqu'on clique en dehors de celui-ci (sur le fond semi-transparent)
   window.addEventListener("click", function (event) {
     if (event.target === modal) {
       closeModal();
@@ -73,22 +90,21 @@ closes.forEach(close => {
 });
 
 // Afficher la seconde partie de la modale :
-  // Récupération de l'élément HTML du bouton "Ajouter une photo"
-  const addPhotoButton = document.querySelector('.add-photo');
-  // Récupération de l'élément HTML de la fenêtre modale pour ajouter un projet
-  const modalAddProject = document.querySelector('#js-modal-add-project');
-  // Ajout d'un gestionnaire d'événement pour empêcher la propagation de l'événement de clic sur la fenêtre modale
-  modalAddProject.addEventListener('click', function (e) {
-    e.stopPropagation();
-  });
-  // Récupération de l'élément HTML de la première fenêtre modale
-  const firstModal = document.querySelector('#js-modal-first');
-  // Ajout d'un gestionnaire d'événement pour afficher la fenêtre modale pour ajouter un projet lorsqu'on clique sur le bouton "Ajouter une photo"
-  addPhotoButton.addEventListener('click', function () {
-    modalAddProject.style.display = null;
-    firstModal.style.display = 'none';
-  });
-
+// Récupération de l'élément HTML du bouton "Ajouter une photo"
+const addPhotoButton = document.querySelector(".add-photo");
+// Récupération de l'élément HTML de la fenêtre modale pour ajouter un projet
+const modalAddProject = document.querySelector("#js-modal-add-project");
+// Ajout d'un gestionnaire d'événement pour empêcher la propagation de l'événement de clic sur la fenêtre modale
+modalAddProject.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+// Récupération de l'élément HTML de la première fenêtre modale
+const firstModal = document.querySelector("#js-modal-first");
+// Ajout d'un gestionnaire d'événement pour afficher la fenêtre modale pour ajouter un projet lorsqu'on clique sur le bouton "Ajouter une photo"
+addPhotoButton.addEventListener("click", function () {
+  modalAddProject.style.display = null;
+  firstModal.style.display = "none";
+});
 
 // Fonction pour fermer la deuxième modal
 function closeSecondModal() {
@@ -97,8 +113,160 @@ function closeSecondModal() {
 }
 
 // Récupération de l'élément de retour à la première modal
-const returnToFirstModalButton = modalAddProject.querySelector(".js-modal-return");
+const returnToFirstModalButton =
+  modalAddProject.querySelector(".js-modal-return");
 // Ajout d'un gestionnaire d'événement pour retourner à la première modal
 returnToFirstModalButton.addEventListener("click", closeSecondModal);
 
-// Coder une fonction pour le fetch avec delete et une autre pour fetch en post 
+// Coder une fonction pour le fetch avec delete et une autre pour fetch en post
+
+function deleteWork(workId) {
+  const token = localStorage.getItem("token"); // Récupérer le jeton d'accès
+  fetch("http://localhost:5678/api/works/" + workId, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // Ajouter le jeton d'accès à l'en-tête
+    },
+  })
+    .then(function (response) {
+      // element supprimer
+      if (response.status == 204) {
+        // proceder à :
+        //1/ mettre à jour le tableau global
+        console.log(works);
+        works = works.filter((element) => element.id != workId);
+        console.log(works);
+
+        //2/Agir sur les figures de la page index et enlever l'element supprimeé depuis le dom
+        document.getElementById("figure" + workId).remove();
+        //3/Eliminer aussi l'element supprimer depuis le dom
+        document.getElementById("figureModale" + workId).remove();
+      }
+      if (response.status == 401) {
+        alert(
+          "Veuillez vous authentifier de nouveau! votre token est expiré !"
+        );
+      }
+    })
+    .catch(function (error) {
+      console.log("Erreur lors de la suppression : " + error.message);
+    });
+}
+  
+
+function removeImage() {
+  const image = document.getElementById("imgPreview");
+  const imageFormDisplay = document.querySelector(".imageFormDisplay");
+  const faimage = document.querySelector(".fa-image");
+  const fileUpload = document.querySelector(".file-upload");
+  const photoInput = document.querySelector("#photo-input");
+
+  imageFormDisplay.style.display = "none";
+  faimage.style.display = "block";
+  fileUpload.style.display = "block";
+  photoInput.style.display = "block";
+  image.src = "";
+}
+
+// Récupérer l'élément d'upload de fichier
+const inputFile = document.querySelector("#photo-input");
+//-------
+inputFile.addEventListener("change", changeImage);
+
+function changeImage(e) {
+  const image = document.getElementById("imgPreview");
+  const imageFormDisplay = document.querySelector(".imageFormDisplay");
+  const faimage = document.querySelector(".fa-image");
+  const fileUpload = document.querySelector(".file-upload");
+  const photoInput = document.querySelector("#photo-input");
+  const fileFormat = document.querySelector(".fileFormat");
+  const remove = document.querySelector(".remove");
+
+  remove.addEventListener("click", function (event) {
+    removeImage();
+  });
+
+  const file = e.target.files[0];
+
+  if (file.type.match("image.*")) {
+    if (file.size <= 4194304) {
+      let reader = new FileReader();
+      reader.onload = function (event) {
+        image.src = event.target.result;
+        imageFormDisplay.style.display = "flex";
+        faimage.style.display = "none";
+        fileUpload.style.display = "none";
+        photoInput.style.display = "none";
+        fileFormat.style.display = "none";
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Le fichier est de grande taille (max 4 Mo).");
+      removeImage();
+    }
+  } else {
+    alert("Le fichier sélectionné n'est pas une image.");
+    removeImage();
+  }
+}
+
+//==== remplacer selected-image par photo input 
+const imageInput = document.getElementById("photo-input");
+const titleInput = document.getElementById("title-photo");
+const categoriesInput = document.getElementById("categories");
+const validerButton = document.getElementById("validerButton");
+
+function validateForm() {
+  const titleValid = titleInput.value.trim() !== "";
+  const categoriesSelected = categoriesInput.value !== "";
+  const imagePreview = document.getElementById("imgPreview");
+
+  if (imagePreview.src !== "" && titleValid && categoriesSelected) {
+    validerButton.classList.add("green-button");
+    return true;
+  } else {
+    validerButton.classList.remove("green-button");
+    return false;
+  }
+}
+
+validateForm();
+
+titleInput.addEventListener("input", validateForm);
+categoriesInput.addEventListener("change", validateForm);
+
+validerButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  if (validateForm()) {
+    const file = imageInput.files[0];
+    const formData = new FormData();
+    formData.append("title", titleInput.value);
+    formData.append("image", file);
+    formData.append("category", categoriesInput.value);
+
+    try {
+      const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + localStorage.getItem("token"),
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        const projet = await response.json();
+        console.log("Projet ajouté avec succès:", projet);
+      } else {
+        console.error("Erreur lors de l'ajout du projet:", response.status);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête:", error);
+    }
+  } else {
+    alert("Veuillez remplir tous les champs.");
+  }
+});
+
+
